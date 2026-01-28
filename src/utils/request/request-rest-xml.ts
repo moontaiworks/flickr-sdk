2842455fb3d90c9bc522b9069a2e83b607b02e15
moontai0724 <moontai0724@gmail.com>
2026-01-28 12:52:19 +0800

    refactor: use ky to request

--- src/utils/request/request-rest-xml.ts
+++ src/utils/request/request-rest-xml.ts
@@ -1,32 +1,33 @@
 import { XMLParser } from "fast-xml-parser";
+import type { SearchParamsOption } from "ky";
+import { default as ky } from "ky";
 
 export interface RequestRestOptions {
   method?: "GET" | "POST";
-  params: URLSearchParams;
+  params: SearchParamsOption;
 }
 
 export async function requestRestXML<T>(options: RequestRestOptions) {
   const endpoint = "https://api.flickr.com/services/rest";
-  const { method = "GET", params } = options;
+  const { method = "GET", params: searchParams } = options;
 
-  const url = new URL(endpoint);
-  url.search = params.toString();
-
-  const request = new Request(url, { method });
-
-  return fetch(request).then(async (response) => {
-    const text = await response.text();
-
-    const parser = new XMLParser({
-      allowBooleanAttributes: true,
-      alwaysCreateTextNode: true,
-      attributeNamePrefix: "",
-      attributesGroupName: false,
-      ignoreAttributes: false,
-      ignoreDeclaration: true,
-    });
-    const xmlDoc = parser.parse(text) as { rsp: T };
-
-    return xmlDoc.rsp;
+  const response = await ky(endpoint, {
+    method,
+    searchParams,
   });
+
+  const text = await response.text();
+  const parser = new XMLParser({
+    allowBooleanAttributes: true,
+    alwaysCreateTextNode: true,
+    attributeNamePrefix: "",
+    attributesGroupName: false,
+    ignoreAttributes: false,
+    ignoreDeclaration: true,
+  });
+
+  const xmlDoc = parser.parse(text) as { rsp: T };
+  if (!("rsp" in xmlDoc)) throw new Error("Invalid XML response!");
+
+  return xmlDoc.rsp;
 }
