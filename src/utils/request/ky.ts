b1cb5184cb95832a96c6e96ffe3301b2653c32a8
moontai0724 <moontai0724@gmail.com>
2026-02-16 02:13:32 +0800

    feat(request): attach api key when present

--- src/utils/request/ky.ts
+++ src/utils/request/ky.ts
@@ -1,7 +1,33 @@
+import type { BeforeRequestHook } from "ky";
 import { default as kyInit } from "ky";
 
 import { parseFlickrXML } from "./xml-parser.js";
 
+interface Context {
+  apiKey?: string;
+}
+
+const attachAuth: BeforeRequestHook = (request, options) => {
+  console.debug("Preparing to attach authentication credentials...");
+  const { apiKey } = options.context as Context;
+
+  if (!apiKey) {
+    console.debug("No API key provided; proceeding without authentication.");
+    return;
+  }
+
+  return attachApiKey(request, apiKey);
+};
+
+function attachApiKey(request: Request, apiKey: string): Request {
+  console.debug("Attaching API key to request...");
+  const url = new URL(request.url);
+  url.searchParams.set("api_key", apiKey);
+  return new Request(url, request);
+}
+
 export const ky = kyInit.extend({
+  hooks: { beforeRequest: [attachAuth] },
   parseJson: parseFlickrXML,
+  prefixUrl: "https://api.flickr.com/services",
 });
