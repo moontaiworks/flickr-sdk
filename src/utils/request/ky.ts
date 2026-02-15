65aa2aa2efbc8f0b92bc3c18cfb46189dfc086e0
moontai0724 <moontai0724@gmail.com>
2026-02-16 02:31:54 +0800

    feat(request): handle both situation of form data and search params

--- src/utils/request/ky.ts
+++ src/utils/request/ky.ts
@@ -1,20 +1,25 @@
-import type { BeforeRequestHook } from "ky";
+import type { BeforeRequestHook, NormalizedOptions } from "ky";
 import { default as kyInit } from "ky";
 
 import { parseFlickrXML } from "./xml-parser.js";
 
 interface Context {
   apiKey?: string;
+  oauth?: {
+    consumerSecret: string;
+  };
 }
 
-const attachAuth: BeforeRequestHook = (request, options) => {
+const attachAuth: BeforeRequestHook = async (request, options) => {
   console.debug("Preparing to attach authentication credentials...");
-  const { apiKey } = options.context as Context;
+  const { apiKey, oauth } = options.context as Context;
 
   if (!apiKey) {
     console.debug("No API key provided; proceeding without authentication.");
     return;
   }
+
+  if (oauth) return attachOAuth(request, options);
 
   return attachApiKey(request, apiKey);
 };
@@ -24,6 +29,50 @@ function attachApiKey(request: Request, apiKey: string): Request {
   const url = new URL(request.url);
   url.searchParams.set("api_key", apiKey);
   return new Request(url, request);
+}
+
+async function attachOAuth(
+  request: Request,
+  options: NormalizedOptions,
+): Promise<Request> {
+  console.debug("Attaching OAuth credentials to request...");
+  const { oauth } = options.context as Context;
+  if (!oauth?.consumerSecret)
+    throw new Error("OAuth consumer secret is required for signing requests.");
+
+  if (request.body instanceof FormData)
+    return handleFormData(request, request.body);
+
+  return handleSearchParams(request);
+}
+
+async function handleFormData(
+  request: Request,
+  body: FormData,
+): Promise<Request> {
+  const url = new URL(request.url);
+  // if body present, force encode all params as URLSearchParams for signing
+  for (const [key, value] of body.entries()) {
+    if (typeof value !== "string") continue; // Skip non-string values for OAuth signing
+    url.searchParams.set(key, value);
+  }
+
+  // TODO: sign the request with OAuth params
+  await Promise.resolve(); // Placeholder for async signing logic
+
+  // Reconstruct body from signed params
+  for (const [key, value] of url.searchParams.entries()) body.set(key, value);
+
+  return new Request(url, { body });
+}
+
+async function handleSearchParams(request: Request) {
+  const url = new URL(request.url);
+
+  // TODO: sign the request with OAuth params
+  await Promise.resolve(); // Placeholder for async signing logic
+
+  return new Request(url);
 }
 
 export const ky = kyInit.extend({
