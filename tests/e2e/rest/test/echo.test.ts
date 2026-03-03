bf3c0016007f3e497858db7d49dff0dfed1f7488
moontai0724 <moontai0724@gmail.com>
2026-03-03 12:44:10 +0800

    test: add e2e test for rest.test.echo with flickr instance

---
+++ tests/e2e/rest/test/echo.test.ts
@@ -0,0 +1,18 @@
+import { createFlickr } from "#index.js";
+
+it("should success response", async () => {
+  const flickr = createFlickr({
+    credentials: {
+      consumerKey: process.env.FLICKR_CONSUMER_KEY!,
+      consumerSecret: process.env.FLICKR_CONSUMER_SECRET!,
+    },
+  });
+  const response = await flickr.rest.test.echo({ hello: "world" });
+
+  expect(response).toStrictEqual({
+    api_key: process.env.FLICKR_CONSUMER_KEY!,
+    hello: "world",
+    method: "flickr.test.echo",
+    stat: "ok",
+  });
+});
