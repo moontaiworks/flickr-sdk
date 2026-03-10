715c945553b6fcfbd53c09f515e03950441c7282
moontai0724 <moontai0724@gmail.com>
2026-03-10 18:27:53 +0800

    feat: implement rest.test.login api endpoint

--- src/apis/rest/test/index.ts
+++ src/apis/rest/test/index.ts
@@ -1,7 +1,9 @@
 export * from "./echo.js";
 
 import { default as echo } from "./echo.js";
+import { default as login } from "./login.js";
 
 export default {
   echo,
+  login,
 };
---
+++ src/apis/rest/test/login.spec.ts
@@ -0,0 +1,21 @@
+import { default as endpoint } from "./login.js";
+
+it("should success response", async () => {
+  const response = await endpoint({
+    consumerKey: process.env.FLICKR_CONSUMER_KEY!,
+    consumerSecret: process.env.FLICKR_CONSUMER_SECRET!,
+    oauthUser: {
+      token: process.env.FLICKR_TOKEN!,
+      tokenSecret: process.env.FLICKR_TOKEN_SECRET!,
+    },
+  });
+
+  expect(response).toStrictEqual({
+    stat: "ok",
+    user: {
+      id: process.env.FLICKR_USER_ID!,
+      path_alias: "",
+      username: process.env.FLICKR_USERNAME!,
+    },
+  });
+});
