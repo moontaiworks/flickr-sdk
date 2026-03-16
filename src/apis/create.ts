3aa6284e783f736d1a78b87354d2981369ffb22c
moontai0724 <moontai0724@gmail.com>
2026-03-17 07:34:01 +0800

    feat: implement flickr.photos.delete endpoint

--- src/apis/create.ts
+++ src/apis/create.ts
@@ -1,6 +1,7 @@
 import createAccessTokenEndpoint from "./auth/access-token.js";
 import createRequestTokenEndpoint from "./auth/request-token.js";
 import type { GeneralOptions } from "./options.js";
+import createFlickrPhotosDelete from "./rest/photos/delete.js";
 import createFlickrTestEcho from "./rest/test/echo.js";
 import createFlickrTestLogin from "./rest/test/login.js";
 import createFlickrTestNull from "./rest/test/null.js";
@@ -10,6 +11,9 @@ export default function createEndpoint(optionsDefault?: GeneralOptions) {
     oauth: {
       accessToken: createAccessTokenEndpoint(optionsDefault),
       requestToken: createRequestTokenEndpoint(optionsDefault),
+    },
+    photos: {
+      delete: createFlickrPhotosDelete,
     },
     test: {
       echo: createFlickrTestEcho(optionsDefault),
