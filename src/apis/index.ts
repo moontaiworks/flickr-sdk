584276f2c741b4353ec6eb6d223638bfc0b211ef
moontai0724 <moontai0724@gmail.com>
2026-03-19 21:33:49 +0800

    fix(type): export types which forgot to export before

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,10 +1,21 @@
 import createEndpoint from "./create.js";
 import type { GeneralOptions } from "./options.js";
 
+export * from "./auth/access-token.js";
 export * from "./auth/construct.js";
-export type * from "./rest/test/echo.js";
-export type * from "./rest/test/login.js";
-export type * from "./rest/test/null.js";
+export * from "./auth/request-token.js";
+export * from "./rest/photos/delete.js";
+export * from "./rest/photos/upload/check-tickets.js";
+export * from "./rest/photosets/add-photo.js";
+export * from "./rest/photosets/create.js";
+export * from "./rest/photosets/delete.js";
+export * from "./rest/photosets/edit-meta.js";
+export * from "./rest/photosets/edit-photos.js";
+export * from "./rest/test/echo.js";
+export * from "./rest/test/login.js";
+export * from "./rest/test/null.js";
+export * from "./upload/replace.js";
+export * from "./upload/upload.js";
 
 export const flickr = createEndpoint();
 
